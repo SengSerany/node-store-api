706a056bd4acc1191f23b4a8a626c0b353014254
SengSerany <cranyseng@gmail.com>
2022-03-25 12:49:49 +0100

    base set up

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+//async errors
+
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// Middleware
+app.use(express.json());
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.send(`<h1>Store API</h1><a href="/api/v1/products">Products route</a>`);
+});
+
+app.use('/api/v1/products', productsRouter);
+
+// Products route
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`-> Server is alive on ${port} !`))
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+start();
