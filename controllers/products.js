206501776e29b4dc384f9c46583a8adacbea50c4
SengSerany <cranyseng@gmail.com>
2022-03-25 13:21:43 +0100

    Connect to the DB and set product Schema

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
 const getAllProductsStatic = async (req, res) => {
+    throw new Error('testing middleware - async_express_errors -')
     res.status(200).json({msg: 'products testing route'});
 };
 
